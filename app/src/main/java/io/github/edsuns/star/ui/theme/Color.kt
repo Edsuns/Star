c1701ac80acd93bee0f0a0c5ec48d9fc2d9a74c5
edsuns <edsuns@qq.com>
2021-09-01 16:04:37 +0800

    Change APP icon

--- app/src/main/java/io/github/edsuns/star/ui/theme/Color.kt
+++ app/src/main/java/io/github/edsuns/star/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package io.github.edsuns.star.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple300 = Color(0xFFCD52FC)
-val Purple600 = Color(0xFF9F00F4)
-val Purple700 = Color(0xFF8100EF)
+val Purple300 = Color(0xFF9DFFCE)
+val Purple600 = Color(0xFF50D18D)
+val Purple700 = Color(0xFF21AD64)
 val Purple800 = Color(0xFF0000E1)
 
 val Red300 = Color(0xFFD00036)
